69636bdd0c590c059ad3ecd881fcb06986b10cb7
Anton Pirker <anton@ignaz.at>
2021-11-09 16:26:31 +0100

    First version of the service

--- timeszones/tests/test_timezones.py
+++ timeszones/tests/test_timezones.py
@@ -2,8 +2,11 @@ import os
 import tempfile
 
 import pytest
+import json
 
 from .. import timezones
+
+TOTAL_TIMEZONES = 413
 
 
 @pytest.fixture
@@ -17,5 +20,32 @@ def client():
 def test_root(client):
     """Test API root"""
 
-    response = client.get("/")
-    assert b'{"timezones":"http://localhost/timezones"}' in response.data
+    rv = client.get("/")
+    assert b'{"timezones":"http://localhost/timezones"}' in rv.data
+
+
+def test_timezone_list(client):
+    """If lat/lon is NOT given a list of all timezones is returned"""
+    rv = client.get("/timezones")
+    data = json.loads(rv.data)
+
+    assert "timezones" in data.keys()
+    assert len(data["timezones"]) == TOTAL_TIMEZONES
+
+
+def test_timezone(client):
+    """If lat/lon is given, a timezone name is returned"""
+    rv = client.get("/timezones?lat=48.19&lon=16.39")
+    data = json.loads(rv.data)
+
+    assert "timezone" in data
+    assert data["timezone"] == "Europe/Vienna"
+
+
+def test_uninhabitated_timezone(client):
+    """If lat/lon is uninhabitated the timezone is UTC"""
+    rv = client.get("/timezones?lat=0&lon=0")
+    data = json.loads(rv.data)
+
+    assert "timezone" in data
+    assert data["timezone"] == "UTC"
--- timeszones/timezones.py
+++ timeszones/timezones.py
@@ -1,4 +1,8 @@
 from flask import Flask, jsonify, request
+from tzwhere import tzwhere
+
+tz = tzwhere.tzwhere()
+
 
 app = Flask(__name__)
 
@@ -10,4 +14,24 @@ def root():
 
 @app.route("/timezones", methods=["GET"])
 def timezones():
-    return jsonify({"hello": f"world"})
+    lat = request.args.get("lat", None)
+    lon = request.args.get("lon", None)
+
+    if not lat and not lon:
+        return jsonify({"timezones": sorted(tz.timezoneNamesToPolygons.keys())})
+
+    if lat and lon:
+        try:
+            timezone_name = tz.tzNameAt(float(lat), float(lon))
+            return jsonify({"timezone": timezone_name or "UTC"})
+        except Exception as e:
+            return (
+                f"[400] BAD REQUEST: Error while fetching timezone for lat/lon: {lat}/{lon}",
+                400,
+            )
+
+    else:
+        return (
+            f"[400] BAD REQUEST: Please specify both query parameters ('lat' AND 'lon') or no query parameter to get a list of all timezones available.",
+            400,
+        )
